a511341751283d235868daf8d6264282398a4686
CristianTalaiguaNRA <cristian.talaigua@numrot.com>
2023-11-27 16:35:09 -0500

    Feat: firts commit Countries part 2.12

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './phonebook/App';
+import App from './countries/App';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
